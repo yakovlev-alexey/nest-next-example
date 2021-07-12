99c29fdeebff68d9e2bd21bd5580f03ce35da568
Alexey Yakovlev <alexeyyakovlev0359@gmail.com>
2021-07-12 23:26:06 +0300

    feat: integrate nest with NEXT.js

--- src/server/app.module.ts
+++ src/server/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { RenderModule } from 'nest-next';
+import Next from 'next';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [RenderModule.forRootAsync(Next({ dev: true }), { viewsDir: null })],
   controllers: [AppController],
   providers: [AppService],
 })
